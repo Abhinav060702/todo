5b17462efb51c635463f9c6fbc848542d121215c
Abhinav060702 <abhinavjasoriya@gmail.com>
2023-11-17 13:53:03 +0530

    Added edit ,isfinished and delete option

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,11 +1,19 @@
-function Todo({text,isFinished}){
+import { useState } from "react";
+
+function Todo({finishTodo,editTodo,deleteTodo,text,isFinished}){
+
+    const [isEditing,setIsEditing]=useState(false);
+    const [todoText,setTodoText]=useState(text);
     return(
 
         <div>
-        <input type="checkbox" checked={isFinished}></input>
-        <span>{text}</span>
-        <button>Edit</button>
-        <button>Delete</button>
+        <input type="checkbox" checked={isFinished} onChange={()=>finishTodo(!isFinished)}></input>
+        {isEditing ? <input value={todoText} onChange={(e)=>setTodoText(e.target.value)}/> : <span>{text}</span>}
+        <button onClick={()=>
+        {setIsEditing(!isEditing);
+        editTodo(todoText);
+        }}>{isEditing?'Save':'Edit'}</button>
+        <button onClick={deleteTodo}>Delete</button>
         </div>
 
     )
--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,10 +1,42 @@
 import  './TodoList.css'
 import Todo from '../Todo/Todo.jsx'
-function TodoList({todos}){
+function TodoList({todos,setTodos}){
+
+    function onDeleteTodo(id){
+        const newTodoList=todos.filter(todo=>todo.id != id);
+        setTodos(newTodoList);
+    }
+
+    function onEditTodo(id,newTodo){
+        const newTodoList=todos.map(todo=>{
+            if(todo.id==id){
+                todo.text=newTodo;
+            }
+            return todo;
+        });
+        setTodos(newTodoList);
+    }
+    function onFinishTodo(id,state){
+
+        const newTodoList=todos.map(todo=>{
+            if(todo.id==id){
+                todo.isFinished=state;
+            }
+            return todo;
+        });
+        setTodos(newTodoList);
+    }
     return(
-        <>
-        {todos && todos.map((todo)=><Todo key={todo.id} text={todo.text} isFinished={todo.isFinished} />)}
-        </>
+        
+        todos && todos.map((todo)=><Todo key={todo.id} 
+        id={todo.id}
+        text={todo.text} 
+        isFinished={todo.isFinished}
+        editTodo={(newTodo)=>onEditTodo(todo.id,newTodo)}
+        deleteTodo={()=>onDeleteTodo(todo.id)}
+        finishTodo={(state)=>onFinishTodo(todo.id,state)}
+        />)
+     
     )
 }
 
